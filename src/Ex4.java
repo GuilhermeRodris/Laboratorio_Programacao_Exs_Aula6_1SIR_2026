c6228d72f32b7e4674c812cf471a2a08920ead00
Guilherme Rodrigues Santos <guilherme-santos2013@hotmail.com>
2026-03-31 15:10:43 -0300

    Adicionado exercícios 4 e 6

---
+++ src/Ex4.java
@@ -0,0 +1,49 @@
+//Média Final do Aluno
+//O professor aplicou durante o semestre duas
+//provas e três trabalhos. A média final será
+//composta por 70% da média das provas e 30%
+//da média dos trabalhos.
+//Escreva um programa em Java que calcule e
+//imprima a média final de um aluno e também a
+//sua situação — aprovado ou reprovado —
+//considerando que a média mínima para
+//aprovação é 6.
+
+import java.util.Scanner;
+
+public class Ex4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double medp,medt,medf, np1, np2, nt1, nt2, nt3;
+
+        System.out.print("Digite a nota da primeira prova: ");
+        np1 = sc.nextDouble();
+        System.out.print("Digite a nota da segunda prova: ");
+        np2 = sc.nextDouble();
+
+        System.out.print("Digite a nota do primeiro projeto: ");
+        nt1 = sc.nextDouble();
+        System.out.print("Digite a nota do segundo projeto: ");
+        nt2 = sc.nextDouble();
+        System.out.print("Digite a nota do terceiro projeto: ");
+        nt3 = sc.nextDouble();
+
+        medp = (np1 + np2) / 2;
+        medt = (nt1 + nt2 + nt3) / 3;
+
+        System.out.printf("A média das provas foi de: %.2f\n", medp);
+        System.out.printf("A média dos trabalhos foi de: %.2f\n", medt);
+
+        medf = (medp * 0.7) + (medt * 0.3);
+
+        System.out.printf("A média final do aluno foi de: %.2f\n", medf);
+
+        if (medf >= 6) {
+            System.out.println("O aluno foi aprovado");
+        }
+
+        else {
+            System.out.println("O aluno foi reprovado");
+        }
+    }
+}
---
+++ src/Ex6.java
@@ -0,0 +1,28 @@
+//Expressão Matemática
+//Desenvolva um programa em Java que calcule
+//e imprima o valor da expressão:
+//Atenção: para o cálculo da expressão, a variável
+//x pode receber qualquer valor? Lembre-se: não
+//existe divisão por zero nem raiz quadrada de
+//número negativo no campo dos números reais.
+
+import java.util.Scanner;
+import static java.lang.Math.pow;
+
+public class Ex6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double x, y;
+
+        System.out.print("Digite o valor da variável X: ");
+        x = sc.nextDouble();
+        if (x <= 0) {
+            System.out.printf("O valor %.2f é invalido como valor para X", x);
+        }
+        else {
+            y = 8 / pow((pow(x,2) - 25), (1.0/2.0));
+
+            System.out.printf("O resutado da equação foi: %.2f", y );
+        }
+    }
+}
